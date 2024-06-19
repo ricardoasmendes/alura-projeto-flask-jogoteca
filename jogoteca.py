25a9488162e1f70f2262b3636d9f872522fc6fda
Ricardo-Alexandre <131568795+Ricardo-Alexandre@users.noreply.github.com>
2024-06-18 21:20:02 -0300

    adicionado opcao de editar e excluir

--- jogoteca.py
+++ jogoteca.py
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
@@ -12,3 +13,19 @@ from views import *
 
 if __name__ == "__main__":
     app.run(debug=True)
+=======
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+
+app = Flask(__name__)
+
+app.config.from_pyfile('config.py')
+db = SQLAlchemy(app)
+
+from views import *
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
+>>>>>>> 94297a76e657a1d2b6fbbd963dc469bd24a6eba1
